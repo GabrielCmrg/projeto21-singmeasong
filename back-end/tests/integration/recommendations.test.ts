3870f8297d2b74fb480c540e7654c53262a51b2f
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-26 19:35:17 -0300

    feat: add test to validate name of recomendation

--- back-end/tests/integration/recommendations.test.ts
+++ back-end/tests/integration/recommendations.test.ts
@@ -20,4 +20,13 @@ describe('POST /recommendations', () => {
       .send(recommendation);
     expect(result.status).toBe(201);
   });
+
+  it("Should fail if the name isn't string", async () => {
+    const recommendation = recommendationFactory.recommendationRequest();
+    delete recommendation.name;
+    const result = await supertest(app)
+      .post('/recommendations')
+      .send(recommendation);
+    expect(result.status).toBe(422);
+  });
 });
