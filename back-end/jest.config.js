cfb8827161d3d9e0639349dce1f459ede027349d
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-26 14:01:58 -0300

    chore: add paths to coverage ignore

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -3,6 +3,17 @@ export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'test-config',
+    'interfaces',
+    'repositories',
+    'jestGlobalMocks.ts',
+    '<rootDir>/src/server.ts',
+    '<rootDir>/src/utils',
+    '<rootDir>/src/database.ts',
+    '<rootDir>/tests/factories',
+  ],
   globals: {
     'ts-jest': {
       useESM: true,
