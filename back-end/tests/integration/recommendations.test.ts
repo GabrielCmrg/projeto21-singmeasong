79d829dd41bb0ee227b17e805e82c3b8835469ec
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-09-26 19:41:53 -0300

    feat: add test to get /recommendations
    This test checks if sending a url that is not from youtube it will return 422.
    Also verifies side effects of a success route.

--- back-end/tests/integration/recommendations.test.ts
+++ back-end/tests/integration/recommendations.test.ts
@@ -18,12 +18,24 @@ describe('POST /recommendations', () => {
     const result = await supertest(app)
       .post('/recommendations')
       .send(recommendation);
+    const recommendations = await prisma.recommendation.findMany();
     expect(result.status).toBe(201);
+    expect(recommendations.length).toBe(1);
+    expect(recommendations[0]).toBeTruthy();
   });
 
   it("Should fail if the name isn't string", async () => {
     const recommendation = recommendationFactory.recommendationRequest();
     delete recommendation.name;
+    const result = await supertest(app)
+      .post('/recommendations')
+      .send(recommendation);
+    expect(result.status).toBe(422);
+  });
+
+  it("Should fail if the url isn't from youtube", async () => {
+    const recommendation = recommendationFactory.recommendationRequest();
+    recommendation.youtubeLink = 'hhtps://google.com';
     const result = await supertest(app)
       .post('/recommendations')
       .send(recommendation);
