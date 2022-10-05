0f6f1a65752c76b1b067b109f0d603dff9c0c06f
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-05 17:50:17 -0300

    test: add route to reset dabase on e2e tests

--- back-end/src/routers/recommendationRouter.ts
+++ back-end/src/routers/recommendationRouter.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { recommendationController } from '../controllers/recommendationController.js';
+import { prisma } from '../database.js';
 
+const env = process.env.START_MODE;
 const recommendationRouter = Router();
 
 recommendationRouter.post('/', recommendationController.insert);
@@ -10,5 +12,12 @@ recommendationRouter.get('/top/:amount', recommendationController.getTop);
 recommendationRouter.get('/:id', recommendationController.getById);
 recommendationRouter.post('/:id/upvote', recommendationController.upvote);
 recommendationRouter.post('/:id/downvote', recommendationController.downvote);
+
+if (env === 'test') {
+  recommendationRouter.delete('/recommendations', async (req, res) => {
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY`;
+    return res.sendStatus(200);
+  });
+}
 
 export default recommendationRouter;
