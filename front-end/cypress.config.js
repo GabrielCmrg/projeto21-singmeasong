622414c41bf41ed492a2b15b1ccb957fb48f038c
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-05 16:15:59 -0300

    chore: configure cypress linting

--- front-end/cypress.config.js
+++ front-end/cypress.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    // eslint-disable-next-line
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
