cf8a02b34bef6c809229292cd54aea5728f1dd26
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-10-05 18:42:51 -0300

    test: adequate test to use the new command

--- front-end/cypress/e2e/recommendation.cy.js
+++ front-end/cypress/e2e/recommendation.cy.js
@@ -32,22 +32,7 @@ describe('recommendation creation', () => {
 describe('recommendation manipulation', () => {
   beforeEach(() => {
     cy.request('DELETE', 'http://localhost:5000/recommendations');
-
-    const NAME_SIZE = 3;
-    const CODE_SIZE = 8;
-    const post = {
-      name: faker.lorem.words(NAME_SIZE),
-      youtubeLink: `https://www.youtube.com/watch?v=${faker.random.alphaNumeric(
-        CODE_SIZE,
-      )}`,
-    };
-    cy.visit('http://localhost:3000');
-
-    cy.get('[data-cy="recomendation-name"]').type(post.name);
-    cy.get('[data-cy="youtube-link"]').type(post.youtubeLink);
-    cy.intercept('POST', '/recommendations').as('recomendationPostRequest');
-    cy.get('[data-cy="post-button"]').click();
-    cy.wait('@recomendationPostRequest');
+    cy.postRecommendation();
   });
 
   it('should upvote', () => {
